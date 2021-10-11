69611d07ab7af9de90dae1f26d6a625b05a1afa2
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2021-10-10 22:25:03 -0700

    feat: add sumneko lua

---
+++ lua/gebhartn/plugins/servers/sumneko.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+local sumneko_binary_path = vim.fn.exepath 'lua-language-server'
+local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
+M.setup = function(lsp_opts)
+    require('lspconfig').sumneko_lua.setup {
+        capabilities = lsp_opts.capabilities,
+        on_attach = function(client)
+            lsp_opts.on_attach(client)
+        end,
+        cmd = { sumneko_binary_path, '-E', sumneko_root_path .. '/main.lua' },
+        root_dir = require('lspconfig').util.root_pattern '.git/',
+        settings = {
+            Lua = {
+                runtime = {
+                    version = 'LuaJIT',
+                    path = runtime_path,
+                },
+                diagnostics = {
+                    globals = { 'vim' },
+                },
+                workspace = {
+                    library = vim.api.nvim_get_runtime_file('', true),
+                },
+                telemetry = {
+                    enable = false,
+                },
+            },
+        },
+    }
+end
+
+return M
